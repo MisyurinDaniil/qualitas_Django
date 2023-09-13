4b7b1aebdabd8caa7d943d5bfc02d0f10fa0b354
MisyurinDaniil_win10_work <d.a.misyurin@gmail.com>
2023-09-13 23:29:02 +0300

    Deleted PROD folder from project.

--- prod/bin/start_gunicorn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /home/qualitas/website/venv/bin/activate
-exec gunicorn -c "/home/qualitas/website/qualitas_store/gunicorn_config.py" config.wsgi
-
--- prod/qualitas_root/gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-command = '/home/qualitas/website/venv/bin/gunicorn'
-pythonpath = '/home/qualitas/website/qualitas_store/'
-bind = '127.0.0.1:8001'
-workers = 3
-user = 'qualitas'
-limit_request_fields = 32000
-limit_request_fields_size = 0
-rav_env = 'DJANGO_SETTINGS_MODULE=config.settings'
