bacbfeb8f3583584879dd014c920ac252850d64d
MisyurinDaniil_win10_work <d.a.misyurin@gmail.com>
2023-10-23 15:31:49 +0300

    Uncommented the code in product View.

--- appOrders/views.py
+++ appOrders/views.py
@@ -28,19 +28,19 @@ def sendTelegram(text = 'Test'):
 
 
 class Makeorder(View):
-        def post(self, request, pk):
-            form = OrderForm(request.POST)
-            if form.is_valid():
-                    # Изменять форму можно только после команды form = form.save(commit=False)
-                    form = form.save(commit=False)
-                    form.order_binding_id = pk
-                    form.save()
-                    text = ('Ссылка на товар - ' + request.POST['order_product_url'] + '\n' + 
-                            'Название товара - ' + ProductItem.objects.get(id=pk).product_name + '\n' + 
-                            'Имя заказчика - ' + request.POST['order_customer_name'] + '\n' + 
-                            'Телефон закачика - ' + request.POST['order_customer_telephone'] + '\n' + '\n' +
-                            'Комментарий к заказу - ' + request.POST['order_customer_comment'])
-                    sendTelegram(text)
-                    return HttpResponse("True")
-            print(form.errors)
-            return HttpResponse("False")
+    def post(self, request, pk):
+        form = OrderForm(request.POST)
+        if form.is_valid():
+                # Изменять форму можно только после команды form = form.save(commit=False)
+                form = form.save(commit=False)
+                form.order_binding_id = pk
+                form.save()
+                text = ('Ссылка на товар - ' + request.POST['order_product_url'] + '\n' + 
+                        'Название товара - ' + ProductItem.objects.get(id=pk).product_name + '\n' + 
+                        'Имя заказчика - ' + request.POST['order_customer_name'] + '\n' + 
+                        'Телефон закачика - ' + request.POST['order_customer_telephone'] + '\n' + '\n' +
+                        'Комментарий к заказу - ' + request.POST['order_customer_comment'])
+                sendTelegram(text)
+                return HttpResponse("True")
+        print(form.errors)
+        return HttpResponse("False")
